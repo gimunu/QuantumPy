c08d33b60514d8b9c9c768dd991df02f40d70460
umberto <umbe@portabilix.local>
2013-09-15 23:12:30 +0200

    propagator to comply Operator class

--- quantumPy/td/evolution.py
+++ quantumPy/td/evolution.py
@@ -25,35 +25,59 @@ from scipy.misc import factorial
 printmsg = messages.print_msg  #shorter name         
 
 class Propagator(Operator):
-    """infinitesimal time propagator"""
+    """Infinitesimal time propagation operator.
+    
+    ...
+    
+    Attributes
+    ----------
+    H : Hamiltonian
+        The hamiltonian operator.
+    type : str
+        The approximation method. Default 'exponential'
+    
+    """
     def __init__(self, Hamiltonian, **kwds):
         super(Propagator, self).__init__(**kwds)
-        self.name = 'Propagator'
+        self.name    = 'Propagator'
+        self.symbol  = 'U(t, t+dt)'
+        self.formula = '\exp(-i dt H)'
         
-        self.Hamiltonian = Hamiltonian
+        self.H    = Hamiltonian
         self.type = kwds.get('Type', 'exponential')
         
         
-    def apply(self, psi, dt, t="0.0"):
+    def applyRigth(self, wfinR, **kwds):
+
+        dt   = kwds.get('dt', 0.01)
+        time = kwds.get('time', 0.00)
         
         if   self.type.lower() == "exponential":
-            exp(psi, self.Hamiltonian, t, dt)
+            exp(wfinR, self.H, time, dt)
         elif self.type.lower() == "aetrs":
             print "not implemented"
         else:
             raise "unknown option"    
 
-    def write_info(self):    
-        printmsg("# Time propagator: ")
-        printmsg("#            type = %s"%(self.type))  
+    def write_info(self, indent = 0): 
+        from functools import partial
+        printmsg = partial(messages.print_msg, indent = indent)       
+        print_msg = messages.print_msg    
+           
+        printmsg("Time propagator:")
+        printmsg(" " ) 
+        print_msg("type = %s"%(self.type), indent = indent+1)  
+        self.H.write_info(indent = indent+1)
+        
 
 
-def exp(psiM, Hop, t, dt, order="5" ): 
+def exp(psiM, Hop, time, dt, order = 5 ): 
     HpsiM = psiM
     UpsiM = psiM.copy()
     UpsiM[:] = 0.0
     for i in range(order):
         UpsiM +=  HpsiM * (- 1j * dt)**i /factorial(i)
-        HpsiM = Hop.apply(HNpsi, t)      
+        HpsiM = Hop.apply(HpsiM)
+        print i, HpsiM      
 
     return UpsiM             
