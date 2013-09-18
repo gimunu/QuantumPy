3cd1bd072326a8cc09374153cbc0ede3f48b717b
umberto <umbe@portabilix.local>
2013-09-18 13:41:07 +0200

    clean up

--- Examples/TestQpy_1.py
+++ Examples/TestQpy_1.py
@@ -28,71 +28,95 @@ sys.path.append('../')
 
 import quantumPy as qp
 
+#
 # First Create the box
-# By default the dimensionality is set to 1 
+# We are working in one-dimension by default
+#
 Radius = 5.0
 box = qp.Box(shape = 'Sphere', radius = Radius, spacing = 0.1)
 box.write_info() # Write a detailed description of the box
 
-# Create an empty  MeshFunction living on the box
+#
+# Create an empty  MeshFunction
+#
 array = np.zeros(box.np)
 wf = qp.grid.MeshFunction( array, mesh = box)
 
+#
 # Create a gaussian wave-packet with initial velocity k
+#
 X = box.points
 k = 4.0
 sigma = np.sqrt(2.0)/k
 wf = qp.grid.MeshFunction( np.pi**(-1.0/4.0) * sigma**(-1.0/2.0) *  
                            np.exp(-X[:]**2.0 / (2.0*sigma**2.0) + 1j*k*X[:]) , mesh = box)
+#
 # Check the normalization
+#
 # print "Norm = %f" % (wf*wf.conjugate()).integrate().real  
 
+#
+# Create the gradient operator \nabla
+# The expectation value on our gaussian packet should be
+# it's momentum k: <- i \nabla> = <P>  
+#
+Grad = qp.system.Gradient(box, Strategy = 'fd')
+# Grad.write_info()
+print "* <P> = %12.6f" % ( (- 1j * Grad.expectationValue(wf)).real)
 
-# Create a Laplace operator
-Grad = qp.system.Gradient(box, Strategy = 'fs', Bc = 'periodic')
-print "<P> = %s" % (Grad.expectationValue(wf))
+#
+# Create the kinetic operator T = -1/2 \nabla^2 
+# The expectation value should be equal to:
+#  <T> = 5/4 <P>^2/2 (= 10 for our gaussian with k = 4 )
+#
+# T = qp.system.operators.Kinetic(box, Strategy = 'fd', Order = 1, Bc = 'periodic')
+T = qp.system.operators.Kinetic(box, Strategy = 'fd', Order = 4, Bc = 'zero')
+print "* <T> = %12.6f" % (T.expectationValue(wf).real)
 
-# Create the kinetic operator 
-# T = qp.system.Kinetic(box, Strategy = 'Fourier')
-T = qp.system.operators.Kinetic(box)
-print "<T> = %s" % (T.expectationValue(wf))
 
 
+#
+# Add the kinetic operator to the list of operators 
+# composing the Hamiltonian.  
+#
 H = qp.system.operators.Hamiltonian(Operators = [T])
 H.write_info()
-print "<H> = %s" % (H.expectationValue(wf))
+print "* <H> = %12.6f" % (H.expectationValue(wf).real)
 
-I = qp.system.operators.Identity()
-print "<I> = %s" % (I.expectationValue(wf))
-
-
-# Calculate kinetic energy in fourier-space
+#
+# Calculate kinetic energy in Fourier-space
+# to check the FS helpers
+#
 Fwf = qp.base.math.rs_to_fs(wf)
 tmp =Fwf.mesh.FSpoints**2.0 * Fwf
 Lwf = qp.base.math.fs_to_rs(tmp, return_mf = True)
 E = 0.5*(wf.conjugate()*Lwf).integrate()
 print "Ek = %s"%(E)
 
-
+#
 # Test operator composition
+#
 OP = qp.system.Operator(Operators = [T,T,Grad])
 OP.write_info()
 
 
 
-
+#
 # Time propagation
 # Create the evolution operator
+#
 U = qp.td.Propagator(H)
 U.write_info()
 
-nt = 2
-dt = 0.005
-# dt = 1j * 0.005
+nt = 500
+dt = 0.001
 
 wft = wf.copy()
 
-anim = 0
+#
+# Plot the evolution of the wavepacket?
+#
+anim = 1
 if anim:
     pl.ion()
     line, =pl.plot(wft.mesh.points, (wft.conjugate()*wft).real)
@@ -105,7 +129,7 @@ print "i        t              <E>"
 for i in range(0,nt):
     wft = U.apply(wft, dt = dt)         # Apply the propagator
     E   = U.H.expectationValue(wft)     # Calculate the energy
-    print "%d\t %1.4f \t%f"%(i, i*dt.real, E.real)
+    print "%d\t %1.4f \t%12.6f"%(i, i*dt.real, E.real)
     if anim:
         line.set_ydata((wft.conjugate()*wft).real)
         pl.draw()
