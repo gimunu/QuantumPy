3c71e6cafe7065ca8b81a5a32981c682e9df91a7
umberto <umberto.degiovannini@gmail.com>
2013-11-10 19:00:16 +0100

    declare classical objects

---
+++ Examples/ClassicParticle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2013 U. De Giovannini
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
+
+import numpy as np
+import pylab as pl
+
+
+import quantumPy as qp
+
+electon = qp.classical.PointParticle(position = [2.0], charge = -1.0)
+
+U =  qp.classical.Propagator(dt = 0.1)
+
+
+print electon
