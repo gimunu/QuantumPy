527258f1cec240eb1981ca7fcde2859a516a821a
umberto <umberto.degiovannini@gmail.com>
2013-11-10 18:59:20 +0100

    added forces and propagation operator

--- quantumPy/classical.py
+++ quantumPy/classical.py
@@ -19,12 +19,84 @@
 
 from __future__ import division
 
-__all__ = ['PointParticle']
+__all__ = ['PointParticle', 'Propagator']
+
+
+import numpy as np
+import copy
 
 
 class PointParticle(object):
     """Classical PointParticle."""
-    def __init__(self, arg):
+    def __init__(self, **kwds):
         super(PointParticle, self).__init__()
+        # The total number of degrees of freedom
+        self.dim   = kwds.get('dim', 1)
+
+        # Physical properties
+        self.mass   = kwds.get('mass', 1.0)
+        self.charge = kwds.get('charge', 1.0)
+
+        # Physical variables 
+        self.currentPos = np.array(kwds.get('position', [0]*self.dim))
+        self.oldPos = self.currentPos.copy()
+
+        self.velocity = np.array(kwds.get('velocity', [0]*self.dim))
+        self.forces   = np.array(kwds.get('forces', [0]*self.dim))
+        
+        # Should the particle be locked at its current position?
+        self.locked = np.array(kwds.get('locked', False))
+
+
+    def __str__(self):
+        return "Particle <M=%s, Q=%s; pos=%s, vel=%s>"%(self.mass, self.charge, self.currentPos, self.velocity)
+
+
+
+class Propagator(object):
+    """Classical time propagator"""
+    def __init__(self, **kwds):
+        super(Propagator, self).__init__()
+        self.dt       = kwds.get('dt', 0.1)
+        # self.forces   = kwds.get('forces', Forces())
+        self.time = 0.0    
+        
+    def verlet(self, points):
+        # Verlet integration step:
+        for p in points:
+            if not p.locked:
+                # make a copy of our current position
+                temp = p.currentPos.copy()
+                p.currentPos += p.currentPos - p.oldPos + p.forces * self.dt**2
+                p.oldPos = temp
+    
+    def accumulateForces(self, particles, **kwds):
+        particles[:].p.forces = 0.0
+        
+        for p in particles:
+            for F in self.forces:
+                p.forces += F.evaluate(p, particles, time)
+    
+    def apply(self, particles, *kdws):
+        self.dt   = get.kdws('dt'  , self.dt)
+        self.time = get.kdws('time', self.time)
+        
+        self.accumulateForces(particles, time)
+        self.verlet()
+        for i in range(ITERATE):
+            self.satisfyConstraints()
+
+
+class Force(object):
+    """docstring for Force"""
+    def __init__(self, arg):
+        super(Force, self).__init__()
+        self.arg = arg
+
+class Forces(object):
+    """docstring for Forces"""
+    def __init__(self, arg):
+        super(Forces, self).__init__()
         self.arg = arg
         
+                    
