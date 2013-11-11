d789552805299fe25f4d7a20b6e5fb29f492a83e
umberto <umberto.degiovannini@gmail.com>
2013-11-11 11:11:30 +0100

    +Forces + initialize particles at time 0

--- quantumPy/classical.py
+++ quantumPy/classical.py
@@ -19,19 +19,34 @@
 
 from __future__ import division
 
-__all__ = ['PointParticle', 'Propagator']
+__all__ = ['PointParticle', 'Propagator', 'Force']
 
 
 import numpy as np
 import copy
+from .base import *
+from .grid import *
 
+
+class SimulationBox(object):
+    """docstring for SimulationBox"""
+    def __init__(self, **kwds):
+        super(SimulationBox, self).__init__()
+        self.dim = kwds.get('dim', 1)
+        self.shape = kwds.get('shape', None)
+        
+    def write_info():
+        pass
+            
 
 class PointParticle(object):
     """Classical PointParticle."""
-    def __init__(self, **kwds):
+    def __init__(self, sbox, **kwds):
         super(PointParticle, self).__init__()
         # The total number of degrees of freedom
-        self.dim   = kwds.get('dim', 1)
+        assert(isinstance(sbox, SimulationBox))
+        self.sb    = sbox
+        self.dim   = self.sb.dim
 
         # Physical properties
         self.mass   = kwds.get('mass', 1.0)
@@ -51,52 +66,91 @@ class PointParticle(object):
     def __str__(self):
         return "Particle <M=%s, Q=%s; pos=%s, vel=%s>"%(self.mass, self.charge, self.currentPos, self.velocity)
 
+    def kinetic_energy(self):
+        return np.dot(self.velocity, self.velocity)/(2.*self.mass)
 
 
 class Propagator(object):
     """Classical time propagator"""
-    def __init__(self, **kwds):
+    def __init__(self, forces, **kwds):
         super(Propagator, self).__init__()
+        self.forces   = forces
         self.dt       = kwds.get('dt', 0.1)
-        # self.forces   = kwds.get('forces', Forces())
         self.time = 0.0    
         
-    def verlet(self, points):
+    def verlet(self, particles):
         # Verlet integration step:
-        for p in points:
+        for p in particles:
             if not p.locked:
                 # make a copy of our current position
                 temp = p.currentPos.copy()
-                p.currentPos += p.currentPos - p.oldPos + p.forces * self.dt**2
+                p.currentPos = 2.*p.currentPos - p.oldPos + p.forces/p.mass * self.dt**2.
                 p.oldPos = temp
+                # Approximated 
+                p.velocity = (p.currentPos - p.oldPos)/self.dt 
     
-    def accumulateForces(self, particles, **kwds):
-        particles[:].p.forces = 0.0
+    def accumulateForces(self, **kwds):
+        particles = kwds.get('particles', None)
+
+        for p in particles:
+            p.forces = np.array([0]*p.dim)
         
         for p in particles:
             for F in self.forces:
-                p.forces += F.evaluate(p, particles, time)
+                p.forces += F.evaluate(p, **kwds)
     
-    def apply(self, particles, *kdws):
-        self.dt   = get.kdws('dt'  , self.dt)
-        self.time = get.kdws('time', self.time)
+    def apply(self, particles, **kwds):
+        self.dt   = kwds.get('dt'  , self.dt)
+        self.time = kwds.get('time', self.time)
         
-        self.accumulateForces(particles, time)
-        self.verlet()
-        for i in range(ITERATE):
-            self.satisfyConstraints()
+        self.accumulateForces(particles = particles, time = self.time )
+        self.verlet(particles)
+        # for i in range(ITERATE):
+        #     self.satisfyConstraints()
+
+    def initialize(self, particles):
+        for p in particles:
+            p.oldPos = p.currentPos - p.velocity *self.dt
+        
 
 
 class Force(object):
-    """docstring for Force"""
-    def __init__(self, arg):
+    """Classical Force class"""
+    def __init__(self, sbox, **kwds):
         super(Force, self).__init__()
-        self.arg = arg
 
-class Forces(object):
-    """docstring for Forces"""
-    def __init__(self, arg):
-        super(Forces, self).__init__()
-        self.arg = arg
+        assert(isinstance(sbox, SimulationBox))
+        self.sb    = sbox
+        self.dim   = self.sb.dim
         
-                    
+        
+        self.name    = kwds.get('name'  , 'Force')
+        self.symbol  = kwds.get('symbol' , "F")
+        self.formula = kwds.get('formula', self.symbol)
+        self.info    = kwds.get('info'   , None)
+        
+
+        self.forces  = []
+        # self.expr    = BinaryTree(self)
+
+        self.forceField = None 
+        
+    def write_info(self, indent = 0): 
+        print_msg( "%s force (%s): "%(self.name, self.symbol), indent = indent )       
+        if self.formula:
+            print_msg( "%s = %s "%(self.symbol, self.formula), indent = indent+1)    
+        if self.info:
+            print_msg( "info: %s "%(self.info), indent = indent+1)    
+    
+    def evaluate(self, particle, **kwds):
+        
+        F = [0]*self.dim
+        
+        if getattr(self, 'forceField'):
+            forceField = getattr(self, forceField)
+            F = forceField(particle.currentPos[:], **kwds)
+        else:
+           pass 
+            # wfout = self._evaluate(self.expr, wfin, side, **kwds)
+        
+        return F                
